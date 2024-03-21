7140074d7e2a652653a44f5e4817220be842a778
이현진 <jakelee@op.gg>
2024-03-21 22:43:11 +0900

    tailwindccss - dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,18 @@
 
 export default function Home() {2
-  return (<main className="bg-gray-300 h-screen flex items-center justify-center p-5">
-<div className="bg-white w-full shadow-lg p-5 rounded-2xl">
+  return (<main className="bg-gray-100 h-screen flex items-center justify-center p-5 dark:bg-gray-700 dark:text-white">
+<div className="bg-white w-full shadow-lg p-5 rounded-3xl max-w-screen-sm dark:bg-gray-600">
   <div className="flex justify-between items-center">
     <div className="flex flex-col">
-      <span className="text-gray-600 font-semibold -mb-1">In transit</span>
-      <span className="text-4xl font-semibold">Coolblue</span>
+      <span className="text-gray-600 font-semibold -mb-1 dark:text-gray-300">In transit</span>
+      <span className="text-4xl font-semibold dark:text-white">Coolblue</span>
     </div>
-    <div className="bg-blue-500 size-12 rounded-full"/>
+    <div className="bg-blue-400 size-12 rounded-full"/>
   </div>
 
   <div className="flex my-3 items-center gap-1">
-    <span className="bg-green-500 text-white px-4 py-1 rounded-full text-xs font-medium">Today</span>
-    <span className="text-sm">9:30-10:30</span>
+    <span className="bg-green-400 text-white px-4 py-1 rounded-full text-xs font-medium hover:bg-green-500 hover:scale-105 cursor-default uppercase transition">Today</span>
+    <span className="text-sm dark:text-gray-200">9:30-10:30</span>
   </div>
 
 <div className="relative">
@@ -20,11 +20,11 @@ export default function Home() {2
   <div className="bg-green-400 w-2/3 h-2 rounded-full absolute"></div>
 </div>
 
-  <div className="flex justify-between items-center mt-5 text-gray-600">
+  <div className="flex justify-between items-center mt-5 text-gray-600 dark:text-gray-300">
     <span>Expected </span>
     <span>Sorting center</span>
     <span>In transit</span>
-    <span className="text-gray-400">Delivered</span>
+    <span className="text-gray-400 dark:text-gray-500">Delivered</span>
   </div>
 
 </div>
