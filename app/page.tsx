f36185affc928f9a89d52212eab738fa9bdd570a
이현진 <jakelee@op.gg>
2024-03-23 00:01:36 +0900

    tailwindcss prettier & invalid, peer

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
 export default function Home() {
   2;
   return (
-    <main className="bg-gray-100 h-screen flex items-center justify-center p-5 ">
-      <div className="bg-white w-full shadow-lg p-5 rounded-3xl max-w-screen-sm flex flex-col gap-3 md:flex-row">
-        <input className="w-full rounded-full h-10 bg-gray-200 pl-5 py-3 outline-none ring ring-transparent focus:ring-orange-500 focus:ring-offset-2 transition-shadow" type="text" placeholder="Search here..." />
-        <button className="bg-black text-white py-2 rounded-full active:scale-95 transition-transform font-medium outline-none md:px-10">Search</button>
+    <main className="flex h-screen items-center justify-center bg-gray-100 p-5 ">
+      <div className="flex w-full max-w-screen-sm flex-col gap-3 rounded-3xl bg-white p-5 shadow-lg md:flex-row">
+        <input
+          className="peer h-10 w-full rounded-full bg-gray-200 py-3 pl-5 outline-none ring ring-transparent transition-shadow focus:ring-green-500 invalid:focus:ring-red-500"
+          type="email"
+          required
+          placeholder="Email address"
+        />
+        <span className="hidden font-medium text-red-500 peer-invalid:block">
+          Email is required.
+        </span>
+        <button className="whitespace-nowrap rounded-full bg-black py-2 font-medium text-white outline-none transition-transform active:scale-95 peer-invalid:bg-gray-300 md:px-10">
+          Log in
+        </button>
       </div>
     </main>
   );
