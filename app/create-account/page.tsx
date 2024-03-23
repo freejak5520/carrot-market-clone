b2e6806c2e4e7ed8861683992c18c46842e5c68f
이현진 <jakelee@op.gg>
2024-03-23 14:07:03 +0900

    create account page

---
+++ app/create-account/page.tsx
@@ -0,0 +1,34 @@
+import { DevicePhoneMobileIcon } from "@heroicons/react/24/outline";
+import { ChatBubbleOvalLeftEllipsisIcon } from "@heroicons/react/24/solid";
+import Link from "next/link";
+
+const CreateAccountPage = () => {
+  return (
+    <div className="flex flex-col gap-10 px-6 py-8">
+      <div className="flex flex-col gap-2 *:font-medium">
+        <h1 className="text-2xl">안녕하세요</h1>
+        <h2 className="text-xl">회원가입을 위해 아래 정보를 입력해주세요</h2>
+      </div>
+      <form className="flex flex-col gap-3">
+        <div className="flex flex-col gap-2">
+          <input className="focus::outline-none h-10 w-full rounded-md border-none bg-transparent ring-1 ring-neutral-200 placeholder:text-neutral-400 focus:ring-2 focus:ring-orange-500" />
+          <span className="text-sm text-red-500">Input error</span>
+        </div>
+        <button className="btn-primary h-10">Create account</button>
+      </form>
+      <div className="h-px w-full bg-neutral-500" />
+      <div className="flex flex-col gap-2">
+        <Link
+          className="btn-primary flex h-10 items-center justify-center gap-2"
+          href="/sms"
+        >
+          <span>
+            <ChatBubbleOvalLeftEllipsisIcon className="size-5" />
+          </span>
+          <span>Sign up with SMS</span>
+        </Link>
+      </div>
+    </div>
+  );
+};
+export default CreateAccountPage;
--- app/page.tsx
+++ app/page.tsx
@@ -12,10 +12,7 @@ export default function Home() {
       </div>
       <div />
       <div className="flex w-full flex-col items-center gap-3">
-        <Link
-          href="/create-account"
-          className="w-full rounded-md bg-orange-500 py-2.5 text-center text-lg font-medium text-white transition-colors hover:bg-orange-400"
-        >
+        <Link href="/create-account" className="btn-primary py-2.5 text-lg">
           시작하기
         </Link>
         <div className="flex gap-2">
